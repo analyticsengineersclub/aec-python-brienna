30ef86d5ebaf2effb1bcde9b4cf65b7b0c7ae394
Brienna Herold <brienna.herold@gmail.com>
2021-12-12 18:14:30 +0000

    Add test to limit number of arguments

--- calc.py
+++ calc.py
@@ -2,18 +2,18 @@
 #     python calc.py add 1 2
 #     python calc.py sub 10 5
 #     python calc.py mult 5 5
+#     python calc.py div 10 5
 
 import argparse
 
 parser = argparse.ArgumentParser(description = "CLI Calculator.")
-
 subparsers = parser.add_subparsers(help = "sub-command help", dest="command")
 
 add = subparsers.add_parser("add", help = "add integers")
 add.add_argument("ints_to_sum", nargs="*", type=int)
 
 sub = subparsers.add_parser("sub", help='subtract integers')
-sub.add_argument("ints_to_sub", nargs=2, type=int)
+sub.add_argument("ints_to_sub", nargs="*", type=int)
 
 mult = subparsers.add_parser("mult", help = "multiply integers")
 mult.add_argument('ints_to_mult', nargs=2, type=int)
@@ -21,24 +21,31 @@ mult.add_argument('ints_to_mult', nargs=2, type=int)
 div = subparsers.add_parser("div", help = "divide integers")
 div.add_argument('ints_to_div', nargs="*", type=int)
 
+
 def aec_subtract(ints_to_sub):
-    arg_1 = ints_to_sub[0]
-    arg_2 = ints_to_sub[1]
-    our_sub = arg_1 - arg_2
-    if our_sub < 0: 
-        our_sub = 0
-    print(f"The subtracted result is: {our_sub}")
-    return our_sub
+    if len(ints_to_sub) > 2: # To catch too many arguments, we need nargs="*"
+        raise TypeError("Too many arguments.")
+    else: 
+        arg_1 = ints_to_sub[0]
+        arg_2 = ints_to_sub[1]
+        our_sub = arg_1 - arg_2
+        if our_sub < 0: 
+            our_sub = 0
+        print(f"The subtracted result is: {our_sub}")
+        return our_sub
+
 
 def aec_add(ints_to_sum):
     our_sum = sum(ints_to_sum)
     print(f"The sum of values is: {our_sum}")
     return our_sum
 
+
 def aec_mult(ints_to_mult):
     our_mult = ints_to_mult[0] * ints_to_mult[1]
     print(f"The multiplied result is: {our_mult}")
     return our_mult
+
 
 def aec_div(ints_to_div):
     if len(ints_to_div) > 2: 
--- tests/test_subtract.py
+++ tests/test_subtract.py
@@ -9,6 +9,9 @@ class TestSubtract(unittest.TestCase):
 		arg_ints = [20, 5]
 		sub_result = aec_subtract(arg_ints)
 		self.assertEqual(sub_result, 15)
+
+	def test_limit_two_args(self):
+		self.assertRaises(TypeError, aec_subtract, [10, 5, 2]) 
 	
 	def test_cant_go_below_zero(self):
 		arg_ints = [5, 20]
