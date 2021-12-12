65cab0261c819d7fddd1969553bb1f9506675d7a
Brienna Herold <brienna.herold@gmail.com>
2021-12-12 16:31:24 +0000

    Correct typos

--- calc.py
+++ calc.py
@@ -12,7 +12,7 @@ subparsers = parser.add_subparsers(help = "sub-command help", dest="command")
 add = subparsers.add_parser("add", help = "add integers")
 add.add_argument("ints_to_sum", nargs=2, type=int)
 
-sub = subparsers.add_parser("sub", help='add integers')
+sub = subparsers.add_parser("sub", help='subtract integers')
 sub.add_argument("ints_to_sub", nargs=2, type=int)
 
 mult = subparsers.add_parser("mult", help = "multiply integers")
@@ -22,7 +22,7 @@ args = parser.parse_args()
 
 if args.command == "add":
     our_sum = sum(args.ints_to_sum)
-    print(f"the sum of values is: {our_sum}")
+    print(f"The sum of values is: {our_sum}")
 elif args.command == "sub":
     our_sub = args.ints_to_sub[0] - args.ints_to_sub[1]
     print(f"The subtracted result is: {our_sub}")
